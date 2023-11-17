bdc7277974292f4324dd88becef717a3b919bbdc
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-17 21:11:21 +0530

    Q8

--- Assigments/2D_array_assigment/Q1.c
+++ Assigments/2D_array_assigment/Q1.c
@@ -1,37 +1,35 @@
 // WAP to input a 2D array of size M*N and display it in tabular form.
-#include<stdio.h>
-int main(){
-    int n , m;
+#include <stdio.h>
+int main()
+{
+    int n, m;
     printf("plz enter no of coloum : ");
-    scanf("%d",&m);
+    scanf("%d", &m);
     printf("plz enter no of row : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[m][n];
-    printf("enter %d values : \n" , m*n);
+    printf("enter %d values : \n", m * n);
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            scanf( "%d", &arr[i][j]);
+            scanf("%d", &arr[i][j]);
         }
-        
     }
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            printf( "arr[%d][%d] = %d \n", i , j ,arr[i][j]);
+            printf("arr[%d][%d] = %d \n", i, j, arr[i][j]);
         }
-        
     }
     printf("\n");
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            printf( " %d \t", arr[i][j]);
+            printf(" %d \t", arr[i][j]);
         }
         printf("\n");
     }
-    
 }
--- Assigments/2D_array_assigment/Q8.c
+++ Assigments/2D_array_assigment/Q8.c
@@ -1,20 +1,65 @@
 // ⦁	WAP to input 2 matrices from the user and add them.
+
 #include <stdio.h>
+
 int main()
 {
-    int m , n;
-    printf("plz enter no of coloum / row : ");
+    int n, m;
+    printf("plz enter no of coloum : ");
     scanf("%d", &m);
-    int arr[m][m];
-    printf("enter %d values : \n", m * m);
-
+    printf("plz enter no of row : ");
+    scanf("%d", &n);
+    int arr[m][n];
+    int arr2[m][n];
+    printf("enter %d values 1st array: \n", m * n);
     for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             scanf("%d", &arr[i][j]);
         }
     }
+    printf("enter %d values of 2ed array: \n", m * n);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr2[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("arr[%d][%d] = %d \n", i, j, arr[i][j]);
+        }
+    }
     printf("\n");
-    
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf(" %d \t", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\t +\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf(" %d \t", arr2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf(" %d \t", arr[i][j] + arr2[i][j]);
+        }
+        printf("\n");
+    }
 }

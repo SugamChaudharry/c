effff0cf39c763a91d0e2dd9cabab3de7036c6a9
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-17 19:28:31 +0530

    2d Array

---
+++ Vinay_Sir_HW/2D_array_assigment/Q1.c
@@ -0,0 +1,28 @@
+// WAP to input a 2D array of size M*N and display it in tabular form.
+#include<stdio.h>
+int main(){
+    int n , m;
+    printf("plz enter no of coloum : ");
+    scanf("%d",&m);
+    printf("plz enter no of row : ");
+    scanf("%d",&n);
+    int arr[m][n];
+    printf("enter %d values : \n" , m*n);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf( "%d", (&arr[i][j]));
+        }
+        
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf( "arr[%d][%d] = %d \n", i , j ,arr[i][j]);
+        }
+        
+    }
+    
+}
---
+++ pyq/2019-2020/Q3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N;
+    printf("Enter N: ");

36709386c561842a4e2e3cae592c2dbd91f7167d
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-19 18:02:18 +0530

    Q1

---
+++ pyq/2019-2020/Q1.c
@@ -0,0 +1,46 @@
+// a) Default massage---------X
+// ans =
+// Default
+// case2
+// case3
+// b) 105-----X --> 13
+// c) 12004-------correct
+
+// # include <stdio.h>
+// void main(){
+//     int a = 5;
+//     switch (a)
+//     {
+//         printf("Message\n");
+//         default: printf("Default\n");
+//     case 2: printf("case2\n");
+//     case 3: printf("case3\n");
+//     }
+// }
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     int a = 15; // Binary: 1111
+//     int b = 7;  // Binary: 0111
+
+//     int result1 = a & b;
+//     int result2 = a | b;
+//     int result3 = a ^ b;
+//     int result4 = ~a;
+
+//     printf("a & b: %d\n", result1);
+//     printf("a | b: %d\n", result2);
+//     printf("a ^ b: %d\n", result3);
+//     printf("~a: %d\n", result4);
+// }
+
+#include <stdio.h>
+int main()
+{
+    int m = 1200;
+    int x = printf("%d", m);
+    printf("%d", x);
+    return 0;
+}

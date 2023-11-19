d19e7c5b83fee7344a83efd282d86b3a382f5c92
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-19 18:02:37 +0530

    Q4

---
+++ pyq/2019-2020/Q4.c
@@ -0,0 +1,27 @@
+//a)
+
+// #include <stdio.h>
+
+// int main(){
+//     int l,w;
+//     scanf("%d", &l);
+//     scanf("%d", &w);
+//     int a = l*w;
+//     int p = 2*(l+w);
+//     printf("area = %d\nperimeter = %d" , a, p);
+// }
+
+// b)
+#include<stdio.h>
+int main(){
+    int i;
+    scanf("%d", &i);
+
+    if (i%2==0)
+    {
+        printf("even");
+    }else{
+        printf("odd");
+    }
+    
+}

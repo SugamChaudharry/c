234b8ae14c7d1abdcf5a6d6409111a34a10e18fe
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-24 08:15:38 +0530

    Q12

---
+++ Assigments/string/Q12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    printf("Enter the string : ");
+    fgets(str, sizeof(str), stdin);
+    printf("in reverse with function :%s\n" , strrev(str));
+    int i=0 , len = strlen(str);
+    char temp;
+    while (str[i]< len-i-1)
+    {
+        temp=str[i];
+        str[i]=str[len-1];
+        str[len-i-1]=temp;
+        i++;
+    }
+    printf("in reverse without function :%s\n" , str);
+}

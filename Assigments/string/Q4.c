c16d6868338cbed982154d91c936388d5c23b695
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-24 06:57:53 +0530

    Q4

--- Assigments/string/Q4.c
+++ Assigments/string/Q4.c
@@ -8,13 +8,29 @@ int main()
     gets(str);
     printf("Enter the string 2: ");
     gets(str2);
-    int cap = strcmp(str, str2);
-    if (cap == 0)
+    // int cap = strcmp(str, str2);
+    // if (cap == 0)
+    // {
+    //     printf("Both strings are equal");
+    // }
+    // else
+    // {
+    //     printf("Both strings are not equal");
+    // }
+    for (int i = 0; str[i] || str2[i] != '\0'; i++)
     {
-        printf("Both strings are equal");
+        if (str[i] != str2[i])
+        {
+            printf("Both strings are not equal");
+            break;
+        }
+        else
+        {
+            printf("Both strings are equal");
+            break;
+        }
+        
     }
-    else
-    {
-        printf("Both strings are not equal");
-    }
+    
+    
 }

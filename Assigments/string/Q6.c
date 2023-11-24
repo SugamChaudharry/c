6e9b2234cdb85b969b6377d3fe0ad123c6757c3b
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-24 07:10:28 +0530

    Q5,6

--- Assigments/string/Q6.c
+++ Assigments/string/Q6.c
@@ -5,13 +5,12 @@ int main()
     char str[100];
     printf("Enter the string 1: ");
     gets(str);
-    for (int i = 0; str[i]!='\0'; i++)
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        if (str[i]>='a' && str[i]<='z')
+        if (str[i] >= 'A' && str[i] <= 'Z')
         {
-            str[i] = str[i] - 32;
+            str[i] = str[i] + 32;
         }
     }
     printf("%s", str);
-    
 }

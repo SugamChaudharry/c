53970959b9a36383e5e6e1db42660add84724d0b
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-29 07:49:31 +0530

    Q15

---
+++ Assigments/string/15Q.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100],find;
+    int i;
+    printf("Enter the string: ");
+    gets(str);
+    printf("Enter the string you want to find: ");
+    scanf("%c",&find);
+    for(i=0;i<strlen(str);i++)
+    {
+        if(str[i]==find)
+        {
+            printf("The index of %c is %d",find,i);
+            break;
+        }
+    }
+        
+    
+    
+}
